0e5c690fd081f35d659b83ad004a0bd0db259307
student <qwe11222017@gmail.com>
2024-11-25 16:56:00 +0800

    v0

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+                    val screenW = resources.displayMetrics.widthPixels
+                    val screenH = resources.displayMetrics.heightPixels
+                    //dp轉像素的倍率 (1dp的像素)
+                    val scale = resources.displayMetrics.density
+                    val game = Game(GlobalScope,screenW, screenH,scale,this)
+                    Start(m = Modifier.padding(innerPadding),game,screenW)
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Start(m: Modifier,game:Game,screenW:Int) {
+    val counter by game.state.collectAsState()
+    var msg by remember { mutableStateOf("遊戲開始") }
+    val boyImage = arrayListOf(
+        R.drawable.boy1, R.drawable.boy2,
+        R.drawable.boy3, R.drawable.boy4, R.drawable.boy5,
+        R.drawable.boy6, R.drawable.boy7, R.drawable.boy8
+    )
+    //繪製小男孩
+    Image(
+        painter = painterResource(id = boyImage[game.boy.pictNo]),
+        contentDescription = "小男孩",
+        modifier = Modifier
+            .width(100.dp)
+            .height(220.dp)
+            .offset { IntOffset(game.boy.x, game.boy.y) }
+    )
+    //繪製病毒
+    val virusImage = arrayListOf(R.drawable.virus1, R.drawable.virus2)
+    Image(
+        painter = painterResource(id = virusImage[game.virus.pictNo]),
+        contentDescription = "病毒",
+        modifier = Modifier
+            .size(80.dp)
+            .offset { IntOffset(game.virus.x, game.virus.y) }
+            .pointerInput(Unit) { //觸控病毒往上，扣一秒鐘
+                detectTapGestures(
+                    onTap = {
+                        game.virus.y -= 40
+                        game.counter -= 25
+                    }
+                )
+            }
+    )
+    Image(
+        painter = painterResource(id = R.drawable.forest),
+        contentDescription = "背景圖",
+        contentScale = ContentScale.FillBounds, //縮放符合螢幕寬度
+        modifier = Modifier
+            .offset { IntOffset(game.background.x1, 0) }
+    )
+    Image(
+        painter = painterResource(id = R.drawable.forest),
+        contentDescription = "背景圖2",
+        contentScale = ContentScale.FillBounds, //縮放符合螢幕寬度
+        modifier = Modifier
+            .offset { IntOffset(game.background.x2, 0) }
+    )
+    if (msg == "遊戲暫停" && !game.isPlaying) {
+        msg = "遊戲結束，按此按鍵重新開始遊戲"
+    }
+    Row {
+        Button(
+            onClick = {
+                if (msg == "遊戲開始" || msg == "遊戲繼續") {
+                    msg = "遊戲暫停"
+                    game.Play()
+                } else if (msg == "遊戲暫停") {
+                    msg = "遊戲繼續"
+                    game.isPlaying = false
+                } else { //重新開始遊戲
+                    msg = "遊戲暫停"
+                    game.Restart()
+                }
+            }
+        ) {
+
+            Text(text = msg)
+        }
+        Text(text = "%.2f 秒".format(counter * .04), modifier = m)
+    }
+    val activity = (LocalContext.current as? Activity)
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Button(
+            onClick = {
+                game.mper1.stop()
+                game.mper2.stop()
+                activity?.finish()
+            }
+        ) {
+            Text("結束App")
+        }
+    }
+}
+
